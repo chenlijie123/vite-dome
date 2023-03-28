decee99eef2e2557212ea8c444e8a6c57844a742
chenlijie <chen.lijie@hxss.com.cn>
2023-03-28 10:58:08 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="notes">
+      <span class="nt-text">2222222</span>
+    </div>
+  </div>
+</template>
+
+ 
+<style lang="scss" scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+.notes {
+  width: 100px;
+  height: 100px;
+  .nt-text{
+    color:red;
+  }
+}
+</style>
